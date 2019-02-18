a15213de854f893032ce3148323f36c10af29db4
Dewa Gede SUgiada <dewadewonah@gmail.com>
2019-02-18 16:42:18 +0700

    add dao trainer

---
+++ dao/trainerDao.js
@@ -0,0 +1,64 @@
+const { trainer } = require('../db/connection');
+const logger = require('../logger/winston');
+
+exports.getAll = function getAll(callback) {
+    studyPeriod.findAll()
+        .then((results) => {
+            return callback(null, results);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
+
+exports.getById = function getById(id, callback) {
+    studyPeriod.findById(id)
+        .then((result) => {
+            return callback(null, result);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
+
+exports.insert = function insert(data, callback) {
+    studyPeriod.create(data)
+        .then((result) => {
+            return callback(null, result);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
+
+exports.update = function update(id, data, callback) {
+    studyPeriod.update(data, {
+        where: { trainerId: data.trainerId },
+        returning: true,
+        plain: true
+    })
+        .then((result) => {
+            logger.info(result);
+            return callback(null, data);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
+
+exports.del = function del(id, callback) {
+    studyPeriod.destroy({
+        where: { trainerId: id }
+    })
+        .then((result) => {
+            return callback(null, id);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
