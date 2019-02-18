fc76332262175701695bcc7ce1439b4c6b14bfc3
Dewa Gede SUgiada <dewadewonah@gmail.com>
2019-02-18 16:14:43 +0700

    add dao studymaterial

---
+++ dao/studyMaterialDao.js
@@ -0,0 +1,64 @@
+const { studyMaterial } = require('../db/connection');
+const logger = require('../logger/winston');
+
+exports.getAll = function getAll(callback) {
+    studyMaterial.findAll()
+        .then((results) => {
+            return callback(null, results);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
+
+exports.getById = function getById(id, callback) {
+    studyMaterial.findById(id)
+        .then((result) => {
+            return callback(null, result);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
+
+exports.insert = function insert(data, callback) {
+    studyMaterial.create(data)
+        .then((result) => {
+            return callback(null, result);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
+
+exports.update = function update(id, data, callback) {
+    studyMaterial.update(data, {
+        where: { weekId: data.weekId },
+        returning: true,
+        plain: true
+    })
+        .then((result) => {
+            logger.info(result);
+            return callback(null, data);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
+
+exports.del = function del(id, callback) {
+    studyMaterial.destroy({
+        where: { weekId: id }
+    })
+        .then((result) => {
+            return callback(null, id);
+        })
+        .catch((error) => {
+            logger.error(error);
+            return callback(error);
+        })
+}
