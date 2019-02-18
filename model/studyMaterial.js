b1eb48cc110751fc6e063efd6f28d7bf3b3bbe04
Dewa Gede SUgiada <dewadewonah@gmail.com>
2019-02-18 16:11:06 +0700

    add model studymaterial

---
+++ model/studyMaterial.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define("study_material", {
+        materialId: {
+            field: "material_id",
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        description: type.TEXT,
+        activeFlag: {
+            field: "active_flag",
+            type: type.ENUM("0", "1"),
+            defaultValue: "1"
+        }
+    }, {
+            tabelName: "study_material",
+            timestamps: false
+        })
+}
