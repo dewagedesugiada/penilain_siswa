8d5da50b7205bbf8f81fcbe1b45022359e479615
Dewa Gede SUgiada <dewadewonah@gmail.com>
2019-02-18 15:00:11 +0700

    add function update and delete

--- controller/studyPeriodController.js
+++ controller/studyPeriodController.js
@@ -8,9 +8,9 @@ exports.List = function (req, res) {
             logger.error('error while select' + err);
             response.err(err, res);
         } else {
-            response.ok(result.res);
+            response.ok(result, res);
         }
-    })
+    });
 }
 
 exports.getDataById = function (req, res) {
@@ -19,9 +19,9 @@ exports.getDataById = function (req, res) {
             logger.error('error while select by id' + err);
             response.err(err, res);
         } else {
-            response.ok(result.res);
+            response.ok(result, res);
         }
-    })
+    });
 }
 
 exports.insertData = function (req, res) {
@@ -30,7 +30,39 @@ exports.insertData = function (req, res) {
             logger.error('error while insert' + err);
             response.err(err, res);
         } else {
-            response.ok(result.res);
+            response.ok(result.affectedRows + "data inserted..", res);
+        }
+    });
+}
+
+exports.updateData = function (req, res) {
+    const body = req.body;
+    studyPeriodDao.getById(body.weekId, function (err, data) {
+        if (err) {
+            logger.error('error while select by id' + err);
+            response.err(err, res);
+        } else if (data == null) {
+            response.datanotfound("data not found ", res);
+        } else {
+            studyPeriodDao.update(body.weekId, body, function (error, result) {
+                if (err) {
+                    logger.error('error while update' + err);
+                    response.err(err, res);
+                } else {
+                    response.ok(result, res);
+                }
+            });
+        }
+    });
+}
+
+exports.deleteData = function (req, res) {
+    studyPeriodDao.del(req.params['id'], function (err, result) {
+        if (err) {
+            logger.error('error while delete' + err);
+            response.err(err, res);
+        } else {
+            response.ok(result, res);
         }
     })
 }
