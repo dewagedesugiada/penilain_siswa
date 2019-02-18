eb95e6c10ea87e9e3685c1cfedbe8093db7db275
Dewa Gede SUgiada <dewadewonah@gmail.com>
2019-02-18 14:17:06 +0700

    edit path route

--- routes/bootcampBatchRoute.js
+++ routes/bootcampBatchRoute.js
@@ -1,9 +1,9 @@
 module.exports = function (app) {
     var controller = require('../controller/bootcampBatchController');
 
-    app.route('bootcampBatch/list').get(controller.List);
-    app.route('bootcampBatch').post(controller.insertData);
-    app.route('bootcampBatch/:id').get(controller.getDataById);
-    app.route('bootcampBatch/').put(controller.updateData);
-    app.route('bootcampBatch/:id').delete(controller.deleteData);
+    app.route('/bootcampBatch/list').get(controller.List);
+    app.route('/bootcampBatch').post(controller.insertData);
+    app.route('/bootcampBatch/:id').get(controller.getDataById);
+    app.route('/bootcampBatch/').put(controller.updateData);
+    app.route('/bootcampBatch/:id').delete(controller.deleteData);
 }
