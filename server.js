d644222fc068756f234271b2ecb3927702ba439c
Dewa Gede SUgiada <dewadewonah@gmail.com>
2019-02-18 14:17:29 +0700

    edit server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3030,
-    bodyParser = require('body-parser'),
-    cors = require('cors'),
-    morgan = require('morgan'),
-    logger = require('./looger/winston');
+    bodyParser = require('body-parser');
+var cors = require('cors');
+var morgan = require('morgan');
+var logger = require('./logger/winston');
 
 
 app.use(bodyParser.json());
@@ -12,6 +12,9 @@ app.use(cors());
 
 app.use(morgan('combined', { "stream": logger.stream }));
 logger.debug("Overriding 'express' logger");
+
+var bootcampBatchRoute = require('./routes/bootcampBatchRoute');
+bootcampBatchRoute(app);
 
 app.listen(port);
 logger.info('RESful API server started on port ' + port);
