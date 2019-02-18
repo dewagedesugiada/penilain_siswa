01717ddf585e49ba715f86321647a62cf623a4b8
Dewa Gede SUgiada <dewadewonah@gmail.com>
2019-02-18 11:45:36 +0700

    add configure server.js

--- server.js
+++ server.js
@@ -2,4 +2,17 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3030,
     bodyParser = require('body-parser'),
-    cors = require('cors');
+    cors = require('cors'),
+    morgan = require('morgan'),
+    logger = require('./looger/winston');
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(morgan('combined', { "stream": logger.stream }));
+logger.debug("Overriding 'express' logger");
+
+app.listen(port);
+logger.info('RESful API server started on port ' + port);
+
