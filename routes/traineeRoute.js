7e22732293e96056e19d9ee0803267d6a80f2e17
Dewa Gede SUgiada <dewadewonah@gmail.com>
2019-02-18 21:27:22 +0700

    add path route trainee

--- routes/traineeRoute.js
+++ routes/traineeRoute.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     var controller = require('../controller/traineeController');
-    app.route('/trainee/list').get(controller.List);
+    app.route('/trainees').get(controller.List);
     app.route('/trainee/').post(controller.insertData);
     app.route('/trainee/:id').get(controller.getDataById);
     app.route('/trainee/').put(controller.updateData);
